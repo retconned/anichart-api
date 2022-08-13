13b6cdfdc4d7bda06c236e2c4fd6eda6f6b54908
retconned <retcnned@gmail.com>
2022-08-13 20:49:36 +0100

    core logic

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,278 @@
+import moment from "moment";
+import axios from "axios";
 import express, { Application, Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 8080;
+type FormatedEntry = {
+    id: number;
+    episode: number;
+    airingAt: string;
+    media: {
+        id: number;
+        title: {
+            romaji: string;
+            native?: string;
+            english?: string;
+        };
+        siteUrl: string;
+        episodes: number;
+        source: string;
+        description: string;
+        coverImage: {
+            extraLarge: string;
+            color: string;
+        };
+    };
+};
 
-app.get('/', (_, res: Response) => {
-    res.send('🚀 Hello World from TypeScript 🚀');
-});
+const app: Application = express();
+const port = process.env.PORT || 3000;
 
+// fetch data from anilist api
+
+async function fetchData(
+    pageNumber: number,
+    weekStart: number,
+    weekEnd: number
+): Promise<any> {
+    try {
+        const query = `
+  {
+    Page(page:${pageNumber} ) {
+       pageInfo {
+        hasNextPage
+      }
+      airingSchedules(airingAt_greater: ${weekStart}, airingAt_lesser: ${weekEnd}) {
+        id
+        episode
+        airingAt
+        media {
+          id
+          title {
+            romaji
+            native
+            english
+          }
+          status
+          format
+          episodes
+          source(version: 2)
+          siteUrl
+          description
+          bannerImage
+          coverImage {
+            extraLarge
+            color
+          }
+        }
+      }
+    }
+  }
+    `;
+        const response = await axios.post("https://graphql.anilist.co/", {
+            query: query,
+        });
+        const data = response.data;
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function scheduleFunc(weekStart: number, weekEnd: number): Promise<any> {
+    let finalArray: any = [];
+
+    let saturdaySchedule: FormatedEntry[] = [];
+    let sundaySchedule: FormatedEntry[] = [];
+    let mondaySchedule: FormatedEntry[] = [];
+    let tuesdaySchedule: FormatedEntry[] = [];
+    let thursdaySchedule: FormatedEntry[] = [];
+    let wednesdaySchedule: FormatedEntry[] = [];
+    let fridaySchedule: FormatedEntry[] = [];
+    try {
+        var x = 0;
+        var test = await fetchData(x, weekStart, weekEnd);
+        var checkNextPage = test.data.Page.pageInfo.hasNextPage;
+        do {
+            finalArray = [...finalArray, ...test.data.Page.airingSchedules];
+            if (checkNextPage) {
+                x++;
+            }
+            test = await fetchData(x, weekStart, weekEnd);
+            checkNextPage = test.data.Page.pageInfo.hasNextPage;
+        } while (checkNextPage);
+
+        // for each loop to generate the schedule for each day
+
+        for (let i = 0; i < finalArray.length; i++) {
+            const entry = finalArray[i];
+            const airingDate = entry.airingAt;
+            var myDate = new Date(airingDate * 1000);
+            var momentDate = moment(myDate).format("dddd");
+            const formatedEntry: FormatedEntry = {
+                id: entry.id,
+                episode: entry.episode,
+                airingAt: entry.airingAt,
+                media: {
+                    id: entry.media.id,
+                    title: {
+                        romaji: entry.media.title.romaji,
+                        native: entry.media.title.native,
+                        english: entry.media.title.english,
+                    },
+                    siteUrl: entry.media.siteUrl,
+                    episodes: entry.media.episodes,
+                    source: entry.media.source,
+                    description: entry.media.description,
+                    coverImage: {
+                        extraLarge: entry.media.coverImage.extraLarge,
+                        color: entry.media.coverImage.color,
+                    },
+                },
+            };
+
+            // sorting days and deleting duplicates
+            switch (momentDate) {
+                case "Saturday":
+                    if (saturdaySchedule.length === 0) {
+                        saturdaySchedule.push(formatedEntry);
+                    } else {
+                        for (let j = 0; j < saturdaySchedule.length; j++) {
+                            if (saturdaySchedule[j].media.id === formatedEntry.media.id) {
+                                break;
+                            } else if (j === saturdaySchedule.length - 1) {
+                                saturdaySchedule.push(formatedEntry);
+                            }
+                        }
+                    }
+                    break;
+                case "Sunday":
+                    if (sundaySchedule.length === 0) {
+                        sundaySchedule.push(formatedEntry);
+                    } else {
+                        for (let j = 0; j < sundaySchedule.length; j++) {
+                            if (sundaySchedule[j].media.id === formatedEntry.media.id) {
+                                break;
+                            } else if (j === sundaySchedule.length - 1) {
+                                sundaySchedule.push(formatedEntry);
+                            }
+                        }
+                    }
+                    break;
+                case "Monday":
+                    if (mondaySchedule.length === 0) {
+                        mondaySchedule.push(formatedEntry);
+                    } else {
+                        for (let j = 0; j < mondaySchedule.length; j++) {
+                            if (mondaySchedule[j].media.id === formatedEntry.media.id) {
+                                break;
+                            } else if (j === mondaySchedule.length - 1) {
+                                mondaySchedule.push(formatedEntry);
+                            }
+                        }
+                    }
+                    break;
+                case "Tuesday":
+                    if (tuesdaySchedule.length === 0) {
+                        tuesdaySchedule.push(formatedEntry);
+                    } else {
+                        for (let j = 0; j < tuesdaySchedule.length; j++) {
+                            if (tuesdaySchedule[j].media.id === formatedEntry.media.id) {
+                                break;
+                            } else if (j === tuesdaySchedule.length - 1) {
+                                tuesdaySchedule.push(formatedEntry);
+                            }
+                        }
+                    }
+                    break;
+                case "Wednesday":
+                    if (wednesdaySchedule.length === 0) {
+                        wednesdaySchedule.push(formatedEntry);
+                    } else {
+                        for (let j = 0; j < wednesdaySchedule.length; j++) {
+                            if (wednesdaySchedule[j].media.id === formatedEntry.media.id) {
+                                break;
+                            } else if (j === wednesdaySchedule.length - 1) {
+                                wednesdaySchedule.push(formatedEntry);
+                            }
+                        }
+                    }
+                    break;
+                case "Thursday":
+                    if (thursdaySchedule.length === 0) {
+                        thursdaySchedule.push(formatedEntry);
+                    } else {
+                        for (let j = 0; j < thursdaySchedule.length; j++) {
+                            if (thursdaySchedule[j].media.id === formatedEntry.media.id) {
+                                break;
+                            } else if (j === thursdaySchedule.length - 1) {
+                                thursdaySchedule.push(formatedEntry);
+                            }
+                        }
+                    }
+                    break;
+                case "Friday":
+                    if (fridaySchedule.length === 0) {
+                        fridaySchedule.push(formatedEntry);
+                    } else {
+                        for (let j = 0; j < fridaySchedule.length; j++) {
+                            if (fridaySchedule[j].media.id === formatedEntry.media.id) {
+                                break;
+                            } else if (j === fridaySchedule.length - 1) {
+                                fridaySchedule.push(formatedEntry);
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+        // push everyday schedule to a single weekly array
+        const weeklyArray = {
+            saturdaySchedule,
+            sundaySchedule,
+            mondaySchedule,
+            tuesdaySchedule,
+            thursdaySchedule,
+            wednesdaySchedule,
+            fridaySchedule,
+        };
+
+        let weeklySchedule = [];
+        // random id generator using uuid
+        function randomIdGenerator() {
+            const id = uuidv4();
+            return id;
+        }
+        const id = randomIdGenerator();
+
+        weeklySchedule.push(
+            { id: id, weekStart: weekStart, weekEnd: weekEnd },
+            weeklyArray
+        );
+
+        return weeklySchedule;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function serveData() {
+    try {
+        app.get("/", async (req: Request, res: Response): Promise<Response> => {
+            // const [weekStart, weekEnd] = dates();
+            const weekStart = moment().startOf("week").unix();
+            const weekEnd = moment().endOf("week").unix();
+            const weeklySchedule = await scheduleFunc(weekStart, weekEnd);
+            return res.status(200).json(weeklySchedule);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+serveData();
 app.listen(port, () => {
-    console.log(`Application is listening at http://localhost:${port}`);
+    console.log(`api is running at http://localhost:${port}`);
 });
